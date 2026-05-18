a2cd6e641210afec24f5b88a5c50b67154982d35
Vinh Tran <vinhtdhe189022@fpt.edu.vn>
2026-05-18 11:55:01 +0700

    Improve lesson details with next previous navigation

--- StudyHub/Pages/Lessons/Details.cshtml.cs
+++ StudyHub/Pages/Lessons/Details.cshtml.cs
@@ -13,6 +13,8 @@ namespace StudyHub.Pages_Lessons
     public class DetailsModel : PageModel
     {
         private readonly StudyHub.Data.ApplicationDbContext _context;
+        public Lesson? PreviousLesson { get; set; }
+        public Lesson? NextLesson { get; set; }
 
         public DetailsModel(StudyHub.Data.ApplicationDbContext context)
         {
@@ -28,16 +30,27 @@ namespace StudyHub.Pages_Lessons
                 return NotFound();
             }
 
-            var lesson = await _context.Lessons.FirstOrDefaultAsync(m => m.Id == id);
+            var lesson = await _context.Lessons
+                .FirstOrDefaultAsync(m => m.Id == id);
 
-            if (lesson is not null)
+            if (lesson == null)
             {
-                Lesson = lesson;
-
-                return Page();
+                return NotFound();
             }
 
-            return NotFound();
+            Lesson = lesson;
+
+            PreviousLesson = await _context.Lessons
+                .Where(l => l.CourseId == lesson.CourseId && l.Id < lesson.Id)
+                .OrderByDescending(l => l.Id)
+                .FirstOrDefaultAsync();
+
+            NextLesson = await _context.Lessons
+                .Where(l => l.CourseId == lesson.CourseId && l.Id > lesson.Id)
+                .OrderBy(l => l.Id)
+                .FirstOrDefaultAsync();
+
+            return Page();
         }
     }
 }
